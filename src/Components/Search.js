6d66e4ea477938ab733259f2340691573ff079b2
chetas11 <cbirmole@gmail.com>
2021-03-22 02:07:42 +0530

    search

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -3,7 +3,13 @@ import React from 'react'
 function Search() {
     return (
         <div>
-            <h3 className="mt-5 text-center">Explore more movies</h3>
+            <h3 className="mt-5 text-center">Explore more movies</h3><br />
+            <div className="text-center p-2 mt-3" style={{background:'white'}}>
+                <input className="p-2 m-2" placeholder="Search Title" />
+                <input className="p-2 m-2" placeholder="Year" />
+                <input className="p-2 m-2" placeholder="ID" />
+                <button className="searchButton">Search</button>
+            </div>
         </div>
     )
 }
