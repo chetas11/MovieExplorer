8d70e7fd5946c2beebd056531144ce3850d81000
chetas11 <cbirmole@gmail.com>
2021-03-22 02:17:34 +0530

    search

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Parademic from '../Images/Parademic.png'
 
 function Search() {
     return (
@@ -9,6 +10,20 @@ function Search() {
                 <input className="p-2 m-2" placeholder="Year" />
                 <input className="p-2 m-2" placeholder="ID" />
                 <button className="searchButton">Search</button>
+            </div>
+            <div className="container mt-5">
+                <div className="row">
+                    <div className="col-lg-4 col-md-4 col-sm-4 col-4">
+                        <div className="row">
+                            <div className="col-lg-6 col-md-6 col-sm-6 col-6">
+                            <img className="img-fluid" src={Parademic}/>
+                            </div>
+                            <div className="col-lg-6 col-md-6 col-sm-6 col-6">
+                                <h4>Inception</h4><br />
+                            </div>
+                        </div>
+                    </div>
+                </div>    
             </div>
         </div>
     )
